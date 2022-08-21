256330ce7160c1a37c35d82b72415cdd021d0f37
rrrrrrttt <3300716736@qq.com>
2022-08-21 14:57:58 +0800

    引入redux-tookit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import ReactDom from 'react-dom/client'
+import { Provider } from 'react-redux'
 
 import App from './App'
+import { store } from './store'
+
 
 const root = ReactDom.createRoot(document.getElementById('root'))
 
-root.render(<App/>)
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
